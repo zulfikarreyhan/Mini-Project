b6660d8956875897e40b3584b00972d5d8c22326
beruang_endul <zulfikar.reyhan@gmail.com>
2022-07-04 21:16:17 +0700

    comment penjelasan

--- Tebak angka.py
+++ Tebak angka.py
@@ -1,29 +1,29 @@
 import random
 
-angka_tertinggi = input("Masukan angka tertinggi : ")
+angka_tertinggi = input("Masukan angka tertinggi : ") # Menentukan angka tertinggi untuk ditebak
 
-if angka_tertinggi.isdigit():
+if angka_tertinggi.isdigit(): # Memastikan angka tertinggi yang dimasukan adalah angka bukan huruf
     angka_tertinggi = int(angka_tertinggi)
-    if angka_tertinggi <= 0 :
+    if angka_tertinggi <= 0 : # Memastikan angka diatas 0
         print("Masukan angka diatas 0 !!!")
         quit()
 else:
     print("Masukan angka!!!")
     quit()
 
-random_angka = random.randint(0, angka_tertinggi)
-banyak_tebakan = 0
+random_angka = random.randint(0, angka_tertinggi) # Membuat angka acak yang harus ditebak
+banyak_tebakan = 0 # variabel banyak nya tebakan yang dilakukan
 
 while True :
-    tebakan = input("Masukan angka tebakan mu : ")
-    if tebakan.isdigit():
+    tebakan = input("Masukan angka tebakan mu : ") # Memasukan angka tebakan
+    if tebakan.isdigit(): # Memastikan angka tebakan adalah angka buka huruf
         tebakan = int(tebakan)
     else:
         print("Harap masukan angka !!!")
         continue
 
-    if tebakan == random_angka :
-        banyak_tebakan += 1
+    if tebakan == random_angka : # Memastikan angka tebakan dan angaka acak sama atau tidak
+        banyak_tebakan += 1 # Menambah angka tebakan ke variabel banyak tebakan.
         print("Tebakan kamu benar !")
         break
     elif tebakan > random_angka:
@@ -31,4 +31,4 @@ while True :
     else:
         print("Tebakan mu dibawah angka")
 
-print("Kamu sudah menebak sebanyak "+str(banyak_tebakan)+ " kali.")
+print("Kamu sudah menebak sebanyak "+str(banyak_tebakan)+ " kali.") # Menampilkan banyak tebakan sampai berhasil sama
--- gunting_kertas_batu.py
+++ gunting_kertas_batu.py
@@ -1,29 +1,28 @@
 import random
 
-from pyparsing import And
-
-pemain_menang = 0
-computer_menang = 0
-suit = ["gunting", 'kertas', 'batu']
+pemain_menang = 0 #Variabel pemain menang suit
+computer_menang = 0 #variabel komputer menang suit
+suit = ["gunting", 'kertas', 'batu'] #list pilihan suit
 
 print("Selamat bermain permainan suit gunting kerts batu.")
 
 while True :
-    pilihan_pemain = input("Masukan pilihan Gunting/Kertas/Batu untuk bermain ATAU Quit/Q untuk keluar : ").lower()
-    if pilihan_pemain == "q":
+    pilihan_pemain = input("Masukan pilihan Gunting/Kertas/Batu untuk bermain ATAU Quit/Q untuk keluar : ").lower() #pemain memilih suit atau keluar dari permainan
+    if pilihan_pemain == "q": #jika memilih keluar akan break
         break
     
     if pilihan_pemain not in suit:
         continue
 
-    random_number = random.randint(0,2)
+    random_number = random.randint(0,2) #suit akan diacak
     # gunting : 0, kertas : 1, batu : 2
 
-    pilihan_computer = suit[random_number]
+    pilihan_computer = suit[random_number] #Menentukan pilihan komputer
     print("Komputer memilih", pilihan_computer+".")
 
-    if pilihan_pemain == "gunting" and pilihan_computer == "kertas":
-        pemain_menang += 1
+    #validasi siapa yang menang suit
+    if pilihan_pemain == "gunting" and pilihan_computer == "kertas": 
+        pemain_menang += 1 #menambah poin ke pemain
         print("Pemain menang !")
     elif pilihan_pemain == "kertas" and pilihan_computer == "batu":
         pemain_menang += 1
@@ -32,9 +31,9 @@ while True :
         pemain_menang += 1
         print("Pemain menang !")
     else:
-        computer_menang += 1
+        computer_menang += 1 #meambah poin ke komputer
         print("Komputer menang !")
-        
-print("Pemain berhasil menang "+ str(pemain_menang)+" kali")
-print("Komputer berhasil menang "+ str(computer_menang)+" kali")
+
+print("Pemain berhasil menang "+ str(pemain_menang)+" kali") #menampilkan poin pemain
+print("Komputer berhasil menang "+ str(computer_menang)+" kali") #menampilkan poin komputer
 print("Selamat tinggal !")
